5e64a91bee914883c54632da2e6427aa7f3de5b9
AmitBiswas1402 <amit.142biswas@gmail.com>
2026-01-30 22:49:50 +0530

    refactor: rename project and update dependencies; add Clerk middleware for route protection
    - Renamed project from "ecomm-self" to "ecomm".
    - Updated dependencies including Next.js, React, and added @clerk/nextjs.
    - Replaced ESLint with Biome for linting and formatting.
    - Introduced a new proxy.ts file to implement Clerk middleware for protecting specific routes.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ["images.unsplash.com", "images.pexels.com", "plus.unsplash.com", "example.com"],
-  },
+  reactCompiler: true,
 };
 
 export default nextConfig;
